e48792244f369b12cf507d9f9665beaadc0f26de
davdkm <davd.km@gmail.com>
2016-07-13 16:04:04 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,31 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	var people = [
+		{
+			name: 'David',
+			favoriteFood: 'Hamburgers'
+		},
+		{
+			name: 'Paul',
+			favoriteFood: 'Pizza'
+		},
+		{
+			name: 'Amy',
+			favoriteFood: 'Dim Sum'
+		},
+		{
+			name: 'Venous',
+			favoriteFood: 'Hamburgers'
+		}
+	];
+
+	it('filters people by Hamburgers', function () {
+		var results = $filter('favoriteFood')(people, 'Hamburgers');
+
+		expect(results.length).toEqual(2);
+		expect(results[1].name).toBe('Venous');
+	})
+
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,15 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
 
+	it('removes all vowels from a string', function () {
+		var str = 'The quick brown fox jumps over the lazy dog.';
+
+		expect(removeAllVowels(str)).toEqual('Th qck brwn fx jmps vr th lzy dg.');
+	})
 });
